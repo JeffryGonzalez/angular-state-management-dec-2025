e742539d2de9f714e78d85cca32f45ce6583a8d4
Jeff Gonzalez <jeff@hypertheory.com>
2025-12-17 03:44:13 +0000

    admin page

--- src/frontend/projects/apps/class/domains/movie-admin/movie-admin.routes.ts
+++ src/frontend/projects/apps/class/domains/movie-admin/movie-admin.routes.ts
@@ -2,10 +2,13 @@ import { Home } from './movie-admin';
 import { HomePage } from './pages/home';
 
 import { FeatureRoutes } from '@app-types/routing/feature-routing';
+import { MovieService } from './stores/movie-service';
+import { movieAdminStore } from './stores/movie-big';
 export const movieAddminRoutes: FeatureRoutes = [
   {
     path: '',
     component: Home,
+    providers: [MovieService, movieAdminStore],
     children: [
       {
         path: '',
